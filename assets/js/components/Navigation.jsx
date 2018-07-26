482a8d7c5df2994535aab89cfe3cecd25fd0d222
mvaleriani <michaelvaleriani@gmail.com>
2018-07-26 12:53:23 -0700

    edit svg

--- assets/js/components/Navigation.jsx
+++ assets/js/components/Navigation.jsx
@@ -4,10 +4,13 @@ class Navigation extends React.Component {
     render() {
         return (
             <nav className="menu">
-                <li id="menu-opt-0" className="menu-opt">0</li>
+                <li id="menu-opt-0" className="menu-opt">
+                    <object type="image/svg+xml" fill="white" data="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPCEtLSBHZW5lcmF0b3I6IEFk%0D%0Ab2JlIElsbHVzdHJhdG9yIDE5LjEuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246%0D%0AIDYuMDAgQnVpbGQgMCkgIC0tPgo8c3ZnIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cu%0D%0AdzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGlu%0D%0AayIgeD0iMHB4IiB5PSIwcHgiIHdpZHRoPSIyNHB4IgoJIGhlaWdodD0iMjRweCIgdmlld0JveD0i%0D%0AMCAwIDI0IDI0IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDAgMCAyNCAyNCIgeG1sOnNwYWNlPSJw%0D%0AcmVzZXJ2ZSI+CjxnIGlkPSJCb3VuZGluZ19Cb3hlcyI+Cgk8ZyBpZD0idWlfeDVGX3NwZWNfeDVG%0D%0AX2hlYWRlcl9jb3B5XzMiPgoJPC9nPgoJPHBhdGggZmlsbD0ibm9uZSIgZD0iTTAsMGgyNHYyNEgw%0D%0AVjB6Ii8+CjwvZz4KPGcgaWQ9Ik91dGxpbmUiPgoJPGcgaWQ9InVpX3g1Rl9zcGVjX3g1Rl9oZWFk%0D%0AZXIiPgoJPC9nPgoJPHBhdGggZD0iTTIwLDNINUMzLjksMywzLDMuOSwzLDV2MTRjMCwxLjEsMC45%0D%0ALDIsMiwyaDE1YzEuMSwwLDItMC45LDItMlY1QzIyLDMuOSwyMS4xLDMsMjAsM3ogTTIwLDV2M0g1%0D%0AVjVIMjB6IE0xNSwxOWgtNXYtOWg1VjE5egoJCSBNNSwxMGgzdjlINVYxMHogTTE3LDE5di05aDN2%0D%0AOUgxN3oiLz4KPC9nPgo8L3N2Zz4K"></object>
+                </li>
                 <li className="menu-opt">1</li>
                 <li className="menu-opt">2</li>
                 <li className="menu-opt">3</li>
+                
             </nav>
         );
     }
