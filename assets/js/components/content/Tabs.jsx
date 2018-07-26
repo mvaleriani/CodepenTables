23851f365f557bb22579fa0a42ba46f43a53ee20
mvaleriani <michaelvaleriani@gmail.com>
2018-07-26 14:45:27 -0700

    filter and tab styling

--- assets/js/components/content/Tabs.jsx
+++ assets/js/components/content/Tabs.jsx
@@ -4,7 +4,10 @@ class Tabs extends React.Component {
     render() {
         return (
             <div id="tabs">
-
+                <div className="tab">TAB 0</div>
+                <div className="tab">TAB 1</div>
+                <div className="tab">TAB 2</div>
+                <div className="tab">TAB 3</div>
             </div>
         );
     }
